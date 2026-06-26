5fdf5e1a23ef0f6d5043e82422d33bd49f068513
cindy grisez <grisezcindy@gmail.com>
2026-06-26 12:26:55 +0200

    feat: add OlympicService as single source of truth

---
+++ src/app/services/olympic.service.ts
@@ -0,0 +1,31 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, shareReplay, map } from 'rxjs/operators';
+import { Olympic } from '../models/olympic.model';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class OlympicService {
+  private readonly olympicUrl = './assets/mock/olympic.json';
+
+  private olympics$: Observable<Olympic[]> = this.http
+    .get<Olympic[]>(this.olympicUrl)
+    .pipe(
+      shareReplay(1),
+      catchError((error) => throwError(() => error))
+    );
+
+  constructor(private http: HttpClient) {}
+
+  getOlympics(): Observable<Olympic[]> {
+    return this.olympics$;
+  }
+
+  getCountryById(id: number): Observable<Olympic | undefined> {
+    return this.olympics$.pipe(
+      map((olympics) => olympics.find((o) => o.id === id))
+    );
+  }
+}
